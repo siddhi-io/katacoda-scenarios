9c2365a78f4b3b69a88bb3745533397290a9fd0e
Suhothayan <suhothayan@gmail.com>
2019-12-16 22:14:50 +0530

    Improve logs

--- siddhi-editor/foreground.sh
+++ siddhi-editor/foreground.sh
@@ -4,7 +4,7 @@ echo "Download siddhi-tooling-5.1.2.zip"
 wget  -q --show-progress https://github.com/siddhi-io/distribution/releases/download/v5.1.2/siddhi-tooling-5.1.2.zip
 
 echo "Unzip siddhi-tooling-5.1.2.zip"
-unzip -q siddhi-tooling-5.1.2.zip
+unzip siddhi-tooling-5.1.2.zip | awk 'BEGIN {ORS=" "} {if(NR%20==0)print "."}'
 
 echo "Start Siddhi Editor"
 ./siddhi-tooling-5.1.2/bin/tooling.sh
